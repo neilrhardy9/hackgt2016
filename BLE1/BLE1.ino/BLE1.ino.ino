fda8cec349fc54705ed480556b1f64245b1c40aa
madeleyne vaca <madeleyne vaca>
2016-09-24 14:34:37 -0400

    added lights, ir, and servo code

--- BLE1/BLE1.ino/BLE1.ino.ino
+++ BLE1/BLE1.ino/BLE1.ino.ino
@@ -1,16 +1,32 @@
 #include <CurieBLE.h>
+#include <Servo.h>
 
 //declare bluetooth
 BLEPeripheral player1;  //first peripheral
 BLEService go("19B10000-E8F2-537E-4F6C-D104768A1214"); //GOGOGOGOGOGOGO
 BLECharCharacteristic HotelIndia("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite | BLENotify);
-BLECentral *bleCentral1 = NULL; // peer central device 
+BLECentral *bleCentral1 = NULL; // peer central device
 
 //declare pin connections
 const int ledPin = 13; // pin to use for the LED
 
+//declare the servos and related variables
+Servo light_flicker;
+int val;
+int pos = 50;
+bool bluetooth = true;
+
 void setup() {
+  light_flicker.attach(9);
+  bluetooth = true;
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, INPUT);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
   Serial.begin(9600);
+  light_flicker.write(20);
   //configure pins
   pinMode(ledPin, OUTPUT);
 
@@ -45,6 +61,7 @@ void buttonPressed(BLECentral & central, BLECharacteristic & characteristic) {
     digitalWrite(ledPin, LOW); //...
   } else {
     digitalWrite(ledPin, HIGH); //GOGOGOGOGOGOGOGOGOGOGOGOGOGOGOGOGOGOGO
+    doTheThing();
   }
 }
 
@@ -60,3 +77,36 @@ void DisconnectHandler(BLECentral & central) {
   Serial.print("Disconnected event, central: ");
   Serial.println(central.address());
 }
+
+int inc = 2;
+int count = 0;
+
+void doTheThing() {
+  Serial.println("trying to do the thing");
+  while (bluetooth) {
+    // put your main  code here, to run repeatedly:
+    if ((pos == 40) || (pos == 130)) {
+      inc = inc * -1;
+    }
+    pos += inc;
+    //Serial.println(pos);
+    light_flicker.write(pos);
+    delay(30);                       // waits 15ms for the servo to reach the position
+    val = analogRead(0);
+    if (val > 300) {
+      count++;
+      Serial.println(count);
+    }
+    digitalWrite(3, LOW);
+    if (count > 10) {
+      digitalWrite(2, HIGH);
+    }
+    if (count > 30) {
+      digitalWrite(3, HIGH);
+    }
+    if (digitalRead(4) == HIGH) {
+      bluetooth = false;
+    }
+  }
+}
+
